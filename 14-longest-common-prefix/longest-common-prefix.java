f6c25320aacf897086f74568728cb82eff52d4c7
Poornachandra_DH <poornachandradh34@gmail.com>
2026-07-24 12:01:17 +0530

    Time: 1 ms (62.16%) | Memory: 42.9 MB (92.38%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -13,8 +13,7 @@ class Solution {
 
             for (int j = 1; j < strs.length; j++) {
 
-                if (i >= strs[j].length() ||
-                    strs[j].charAt(i) != ch) {
+                if (i>=strs[j].length() || strs[j].charAt(i) != ch) {
 
                     return first.substring(0, i);
                 }
