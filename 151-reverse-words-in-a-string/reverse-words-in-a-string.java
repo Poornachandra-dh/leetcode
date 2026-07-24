4bf4eff7b03e453404174e881d2e86ec64bbb829
Poornachandra_DH <poornachandradh34@gmail.com>
2026-07-24 10:22:20 +0530

    Time: 5 ms (84.52%) | Memory: 44.6 MB (37.55%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,60 +1,19 @@
 class Solution {
-
     public String reverseWords(String s) {
+
+        String[] words = s.trim().split("\\s+");
 
         StringBuilder ans = new StringBuilder();
 
-        int n = s.length();
+        for(int i = words.length - 1; i >= 0; i--) {
 
-        // reverse full string
-        for (int i = n - 1; i >= 0; i--) {
-            ans.append(s.charAt(i));
-        }
+            ans.append(words[i]);
 
-        int i = 0;
-
-        while (i < ans.length()) {
-
-            while (i < ans.length() && ans.charAt(i) == ' ') {
-                i++;
-            }
-
-            int j = i;
-
-            while (j < ans.length() && ans.charAt(j) != ' ') {
-                j++;
-            }
-
-            int a = i;
-            int b = j - 1;
-
-            while (a < b) {
-
-                char temp = ans.charAt(a);
-
-                ans.setCharAt(a, ans.charAt(b));
-                ans.setCharAt(b, temp);
-
-                a++;
-                b--;
-            }
-
-            i = j;
-        }
-
-        String[] words = ans.toString().trim().split("\\s+");
-
-        String res = "";
-
-        for (int k = 0; k < words.length; k++) {
-
-            res += words[k];
-
-            if (k != words.length - 1) {
-                res += " ";
+            if(i != 0) {
+                ans.append(" ");
             }
         }
 
-        return res;
+        return ans.toString();
     }
 }
