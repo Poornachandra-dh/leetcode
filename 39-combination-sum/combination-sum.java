53db2f5a3b885c9b17d068aede189547f7330efa
Poornachandra_DH <poornachandradh34@gmail.com>
2026-07-26 17:19:32 +0530

    Time: 2 ms (96.47%) | Memory: 45.5 MB (92.40%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        backtrack(0, candidates, target, list);
+
+        return ans;
+    }
+
+    public void backtrack(int index, int[] candidates, int target, List<Integer> list) {
+
+        // Target achieved
+        if (target == 0) {
+
+            List<Integer> temp = new ArrayList<Integer>();
+
+            for (int i = 0; i < list.size(); i++) {
+                temp.add(list.get(i));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        // Invalid case
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+
+        // Choose current number
+        list.add(candidates[index]);
+
+        backtrack(index, candidates, target - candidates[index], list);
+
+        // Backtrack
+        list.remove(list.size() - 1);
+
+        // Skip current number
+        backtrack(index + 1, candidates, target, list);
+    }
+}
