832f58bb4697c10822abaa19c33f6b1c7482c7d0
Poornachandra_DH <poornachandradh34@gmail.com>
2026-07-30 00:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.12%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,57 +1,43 @@
+
+
 /*class Solution {
+    public int paths(int m , int n , int[][]dp ){
+        if(m==1 && n==1) return 1;
+        if(m==0 || n==0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n] ; 
+        int rightways =  paths(m,n-1,dp);
+        int downways = paths(m-1,n,dp) ; 
+        return dp[m][n] = rightways+downways ; 
 
-    public int uniquePaths(int m, int n) {
-        return path(0, 0, m, n);
     }
 
-    public int path(int row, int col, int m, int n) {
-
-        if (row == m - 1 && col == n - 1)
-            return 1;
-
-        if (row >= m || col >= n)
-            return 0;
-
-        int rightWays = path(row, col + 1, m, n);
-        int downWays = path(row + 1, col, m, n);
-
-        return rightWays + downWays;
-    }
-}
-*/
-
-class Solution {
-
-    int[][] dp;
+    
 
     public int uniquePaths(int m, int n) {
-
-        dp = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][]dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j] = -1 ; 
+            }
         }
+        
+        return paths(m,n,dp);
 
-        return path(0, 0, m, n);
+    }
+} */
+
+class Solution{
+    public int uniquePaths(int m, int n){
+        int [][]dp = new int[m][n] ; 
+        for(int i=0 ; i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0)  dp[i][j]=1;
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1] ; 
+            }
+        }
+        return dp[m-1][n-1] ; 
     }
 
-    public int path(int row, int col, int m, int n) {
-
-        if (row == m - 1 && col == n - 1)
-            return 1;
-
-        if (row >= m || col >= n)
-            return 0;
-
-        // Already computed
-        if (dp[row][col] != -1)
-            return dp[row][col];
-
-        int rightWays = path(row, col + 1, m, n);
-        int downWays = path(row + 1, col, m, n);
-
-        dp[row][col] = rightWays + downWays;
-
-        return dp[row][col];
-    }
 }
+
+    
