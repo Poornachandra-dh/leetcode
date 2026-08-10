40450a02d91339f617e12ac83f6bc1335cd9618e
Poornachandra_DH <poornachandradh34@gmail.com>
2026-08-10 20:36:12 +0530

    Time: 3 ms (86.02%) | Memory: 42.9 MB (93.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            
+            if (ch == '(' || ch == '{' || ch == '[') {
+
+                stack.push(ch);
+
+            } 
+            // Closing brackets
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        
+        return stack.isEmpty();
+    }
+}
