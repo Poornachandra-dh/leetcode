2cd160a3266fb12c00000622856b3b0a7916bb71
Poornachandra_DH <poornachandradh34@gmail.com>
2026-08-28 21:24:16 +0530

    Time: 53 ms (83.01%) | Memory: 21.3 MB (44.10%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        sum=0
+        maxi=nums[0]
+        for i in range(len(nums)):
+            sum+=nums[i]
+            maxi = max(maxi,sum)
+            if sum<0:
+                sum=0
+        return maxi
+        
