219e1720e6afdf03a633aff59d8e9b1279323646
Poornachandra_DH <poornachandradh34@gmail.com>
2026-08-28 21:10:28 +0530

    Time: 785 ms (28.62%) | Memory: 19 MB (26.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,7 +4,7 @@ class Solution:
         result = []
 
         for i in range(len(nums) - 2):
-            # Skip duplicate first elements
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
@@ -12,25 +12,27 @@ class Solution:
             right = len(nums) - 1
 
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
 
-                if total == 0:
+                sum = nums[i] + nums[left] + nums[right]
+
+                if sum == 0:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    # Skip duplicates
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
+
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
 
                     left += 1
                     right -= 1
 
-                elif total < 0:
+                elif sum < 0:
                     left += 1
+
                 else:
                     right -= 1
 
-        return result    
+        return result
 
         
