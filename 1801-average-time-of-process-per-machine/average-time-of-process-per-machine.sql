ab70367845a5ad47b8ecf23d0fde0a1da605b4f4
Poornachandra_DH <poornachandradh34@gmail.com>
2026-08-30 19:37:06 +0530

    Time: 229 ms (97.65%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -4,4 +4,4 @@ JOIN Activity a2
 ON a1.machine_id = a2.machine_id AND  a1.process_id=a2.process_id
 WHERE a1.activity_type = 'start'
   AND a2.activity_type = 'end'
-GROUP BY a2.machine_id;
+GROUP BY a1.machine_id;
